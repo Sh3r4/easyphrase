ddd33cad0d7d1137eaf6d2d88a89ca7e3ac7722a
Morgan Timms <morgan@timms.io>
2017-05-27 11:39:03 +1000

    Bump npm version to release

--- src/cliRun.ts
+++ src/cliRun.ts
@@ -1,9 +1,10 @@
 // parse the commandline
 let runs:number = 1;
 let lessSecure = false;
+let version = require('../package.json').version;
 
 program
-  .version('0.0.2-0')
+  .version(version)
   .option('-s, --silent', 'Output only the generated password')
   .option('-q, --quantity <n>', 'The quantity of passwords to output, as an integer', parseInt)
   .option('-l, --less-secure', 'Do not include the randomly placed integer')
